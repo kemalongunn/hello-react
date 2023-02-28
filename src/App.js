9f4b0074d48bfd435ae2ff1a8b24403b6782c5b3
kemal.ongun <kemal.ongun@aurorabilisim.com>
2023-02-28 14:50:36 +0300

    props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from "react";
+import User from "./Components/User";
 function App() {
-  const name = "Kemal"
-  const surname = "Ongun"
-  const age = 25
+  const name = "Kemal";
+  const surname = "Ongun";
+  const age = 25;
   const isLoggedIn = false;
-
+  const friends = [
+    {
+      id: 1,
+      name:"Kemal"
+    },
+    {
+      id: 2,
+      name:"Ahmet"
+    },
+    {
+      id: 3,
+      name:"Abdulah"
+    }
+  ];
   return (
     <>
-
-      <h1> {isLoggedIn ?`Adım ${name}, soyadım ${surname} yasım ${age}`: "giriş yapmadınız"} </h1>
-
+      <User name={"kemal"} surname={"ongun"} age={age} isLoggedIn={true}
+      friends={friends}
+      />
     </>
   );
 }
---
+++ src/Components/User.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function User({ name, surname, isLoggedIn, age,friends }) {
+  return (
+    <div>
+
+      <h2>
+        {isLoggedIn ? `${name} ${surname} (${age}) ` : "Giriş yapmadiniz..."}
+      </h2>
+
+     {
+        friends.map((friend) => <div key={friend.id}> {friend.id}-{friend.name} </div>)
+    }
+
+    </div>
+  );
+}
+
+export default User;
